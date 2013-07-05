0d0512c422b280bbf9b82e984d4d49bbfc9ecda1
Mako <zephilou@cyklodev.com>
2013-07-05 15:22:04 +0000

    Add time between operation

--- raspmap.sh
+++ raspmap.sh
@@ -18,6 +18,7 @@ then
 fi
 
 RETVAL=0
+TIME_START=`date  +'%s'`
 
 function raspmap(){
 	echo -e "
@@ -27,39 +28,53 @@ function raspmap(){
 		</head>
 		<body style='text-align:left;margin:50px 0px; padding:0px;'>
 	" >$RESULTSFILE
+
 	echo -e "<b>RaspMap </b>: ".`date`."<br />" >>$RESULTSFILE
 	echo "<b>Admimail </b> : $EMAIL  <br />"  >>$RESULTSFILE
 	echo "<b>Public ip </b> of installer is : $PUBLICIP"."<br />" >> $RESULTSFILE
 	echo "<hr>" >>$RESULTSFILE;
 	echo "<b>Local network  </b>:<br /><br />" >> $RESULTSFILE
-
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 	ifconfig >> $RESULTSFILE
-
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
+	
 	echo "<br /><br /><b>Network to scan </b>  : <br />" >>$RESULTSFILE
 
-	ip addr show | grep inet | grep -v 127.0.0.1 | sed -n '/inet/,/brd/p' | awk -F 'inet' '{print $2}'  | awk -F ' ' '{print $1}' >>$RESULTSFILE
 	ip addr show | grep inet | grep -v 127.0.0.1 | sed -n '/inet/,/brd/p' | awk -F 'inet' '{print $2}'  | awk -F ' ' '{print $1}'  > $NETSCANFILE
+	cat $NETSCANFILE >> $RESULTSFILE
 
 	echo "<hr>" >>$RESULTSFILE;
 	echo "<br /><br /><b>Bluetooth check</b>  : <br />" >>$RESULTSFILE
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 	
 	hciconfig >>$RESULTSFILE
 
 	echo "<br /><br /><b>Bluetooth scan</b>  : <br />" >>$RESULTSFILE
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 
 	hcitool scan >>$RESULTSFILE
 
 	echo "<hr>" >>$RESULTSFILE;
 	echo "<br /><br /><b>Wifi check</b>  : <br />" >>$RESULTSFILE
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 
 	iwconfig >>$RESULTSFILE
 
 	echo "<br /><br /><b>Wifi scan</b>  : <br />" >>$RESULTSFILE
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 
 	iwlist wlan0 scan | grep 'Cell\|Frequency\|Quality\|ESSID' | sed 's/Cell/Cell<br \/>/g' >>$RESULTSFILE
 	
 	echo "<hr>" >>$RESULTSFILE;
 	echo "<br /><br /><b>Mtr to google </b>  : <br />" >>$RESULTSFILE
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 	echo "<br /><br />" >>$RESULTSFILE
 
 	mtr -r -c 1  8.8.8.8 | sed 's/^[0-9]\.\|--/<br \/>/g'>>$RESULTSFILE
@@ -69,6 +84,8 @@ function raspmap(){
 	echo "<br /><br /><b>Get localname </b>  : <br />" >>$RESULTSFILE
 
 	while read line ;do
+		TIME_INTER=`date  +'%s'`
+		echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 		nbtscan -qre $line >>$RESULTSFILE
 	done < $NETSCANFILE
 
@@ -77,13 +94,19 @@ function raspmap(){
 
 	while read line ;do
 		echo "Result of  scan <b> $line </b>: <br />" >>$RESULTSFILE
+		TIME_INTER=`date  +'%s'`
+		echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 		nmap -PN $line -oX $NMAPXML
 		xsltproc $NMAPXML  >>$RESULTSFILE
 	done < $NETSCANFILE
 
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 
 	mail -s "RaspMap Boot email $HOSTNAME" -a "MIME-Version: 1.0" -a "Content-Type: text/html" $EMAIL < $RESULTSFILE 
 
+	TIME_INTER=`date  +'%s'`
+	echo ">>>> ".`expr $TIME_INTER - $TIME_START `." sec <br /><br />" >> $RESULTSFILE
 
 	rm -v RESULTSFILE
 	rm -v NETSCANFILE
